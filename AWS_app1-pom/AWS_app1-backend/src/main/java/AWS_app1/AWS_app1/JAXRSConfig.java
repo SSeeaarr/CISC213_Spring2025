5768de65f4c1c05852648e93a836383a5b288a52
SSeeaarr <searjaan24@gmail.com>
2025-02-20 21:39:40 -0500

    initial push

---
+++ AWS_app1-pom/AWS_app1-backend/src/main/java/AWS_app1/AWS_app1/JAXRSConfig.java
@@ -0,0 +1,9 @@
+package AWS_app1.AWS_app1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfig extends Application {
+
+}
